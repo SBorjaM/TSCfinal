4012d7b49381137bb8bcfb3505bd165078063e6b
Sebastian B <sebastianborja@Sebastians-MacBook-Pro.local>
2019-11-27 20:25:37 -0300

    animation

---
+++ TheSillyCompany/blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('index.html', views.index, name='index'),
+    path('u-desk.html', views.udesk, name='u-desk'),
+    path('curtain.html', views.curtain, name='curtain'),
+    path('armband.html', views.armband, name='armband'),
+    path('nosotros.html', views.nosotros, name='nosotros'),
+]
---
+++ TheSillyCompany/blog/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+# Create your views here.
+def index(request):
+    return render(request, 'blog/index.html', {})
+
+def udesk(request):
+    return render(request, 'blog/u-desk.html', {})
+
+def curtain(request):
+    return render(request, 'blog/curtain.html', {})
+
+def armband(request):
+    return render(request, 'blog/armband.html', {})
+
+def nosotros(request):
+    return render(request, 'blog/nosotros.html', {})
