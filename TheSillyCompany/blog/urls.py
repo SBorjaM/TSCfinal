0c025f208a1998c440a4a833280663cf2609e644
Sebastian B <sebastianborja@Sebastians-MacBook-Pro.local>
2019-11-28 18:47:29 -0300

    all

--- TheSillyCompany/blog/urls.py
+++ TheSillyCompany/blog/urls.py
@@ -1,3 +1,18 @@
+from django.conf.urls import url, include
+from rest_framework import routers
+from blog.quickstart import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
+
 from django.urls import path
 from django.conf.urls import include, url
 from . import views
@@ -15,17 +30,4 @@ urlpatterns = [
     url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit')
 ]
 
-from django.conf.urls import url, include
-from rest_framework import routers
-from blog.quickstart import views
 
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
-
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
-urlpatterns = [
-    url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-]
