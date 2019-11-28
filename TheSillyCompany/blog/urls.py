1a81211856ed456aed8052ff7ee72c8152b15387
Sebastian B <sebastianborja@Sebastians-MacBook-Pro.local>
2019-11-28 16:17:59 -0300

    posts

--- TheSillyCompany/blog/urls.py
+++ TheSillyCompany/blog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
@@ -8,5 +9,8 @@ urlpatterns = [
     path('curtain.html', views.curtain, name='curtain'),
     path('armband.html', views.armband, name='armband'),
     path('nosotros.html', views.nosotros, name='nosotros'),
-    path('comunidad.html', views.comunidad, name='comunidad')
+    path('comunidad.html', views.comunidad, name='comunidad'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit')
 ]
