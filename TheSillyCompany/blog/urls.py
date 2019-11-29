6f8cd5209178963e744f067eda73547c89881fb8
Sebastian B <sebastianborja@Sebastians-MacBook-Pro.local>
2019-11-28 22:10:15 -0300

    master merge

--- TheSillyCompany/blog/urls.py
+++ TheSillyCompany/blog/urls.py
@@ -1,4 +1,20 @@
+from django.conf.urls import url, include
+from rest_framework import routers
+from blog.quickstart import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
+
 from django.urls import path
+from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
@@ -8,4 +24,10 @@ urlpatterns = [
     path('curtain.html', views.curtain, name='curtain'),
     path('armband.html', views.armband, name='armband'),
     path('nosotros.html', views.nosotros, name='nosotros'),
+    path('comunidad.html', views.comunidad, name='comunidad'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit')
 ]
+
+
