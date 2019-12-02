ba389f6113b6ca0f3285e299bd3f867ff157c38a
Sebastian B <sebastianborja@Sebastians-MacBook-Pro.local>
2019-12-02 17:33:46 -0300

    final

--- TheSillyCompany/blog/urls.py
+++ TheSillyCompany/blog/urls.py
@@ -1,21 +1,19 @@
 from django.conf.urls import url, include
 from rest_framework import routers
-from blog.quickstart import views
-
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
-
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
-urlpatterns = [
-    url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-]
+from blog.quickstart import viewsAPI
 
 from django.urls import path
 from django.conf.urls import include, url
 from . import views
+
+router = routers.DefaultRouter()
+router.register(r'users', viewsAPI.UserViewSet)
+router.register(r'groups', viewsAPI.GroupViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API
+
+
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -25,9 +23,19 @@ urlpatterns = [
     path('armband.html', views.armband, name='armband'),
     path('nosotros.html', views.nosotros, name='nosotros'),
     path('comunidad.html', views.comunidad, name='comunidad'),
+    #login
+    path('welcome.html', views.welcome),
+    path('register', views.register),
+    path('login', views.login),
+    path('logout', views.logout),
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
-    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit')
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    
+
+    #path('admin/', admin.site.urls),
 ]
 
 
--- TheSillyCompany/blog/views.py
+++ TheSillyCompany/blog/views.py
@@ -4,6 +4,12 @@ from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 from django.shortcuts import redirect
+#login 
+from django.contrib.auth import logout as do_logout
+from django.contrib.auth import authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as do_login
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def index(request):
@@ -55,3 +61,70 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('comunidad')
+
+#login
+
+def welcome(request):
+    # Si estamos identificados devolvemos la portada
+    if request.user.is_authenticated:
+        return render(request, "blog/welcome.html")
+    # En otro caso redireccionamos al login
+    return redirect('/login')
+
+def register(request):
+    # Creamos el formulario de autenticación vacío
+    form = UserCreationForm()
+    if request.method == "POST":
+        # Añadimos los datos recibidos al formulario
+        form = UserCreationForm(data=request.POST)
+        # Si el formulario es válido...
+        if form.is_valid():
+
+            # Creamos la nueva cuenta de usuario
+            user = form.save()
+
+            # Si el usuario se crea correctamente 
+            if user is not None:
+                # Hacemos el login manualmente
+                do_login(request, user)
+                # Y le redireccionamos a la portada
+                return redirect('/')
+
+    # Si llegamos al final renderizamos el formulario
+    return render(request, "blog/register.html", {'form': form})
+
+def login(request):
+    # Creamos el formulario de autenticación vacío
+    form = AuthenticationForm()
+    if request.method == "POST":
+        # Añadimos los datos recibidos al formulario
+        form = AuthenticationForm(data=request.POST)
+        # Si el formulario es válido...
+        if form.is_valid():
+            # Recuperamos las credenciales validadas
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            # Verificamos las credenciales del usuario
+            user = authenticate(username=username, password=password)
+
+            # Si existe un usuario con ese nombre y contraseña
+            if user is not None:
+                # Hacemos el login manualmente
+                do_login(request, user)
+                # Y le redireccionamos a la portada
+                return redirect('comunidad.html')
+
+    # Si llegamos al final renderizamos el formulario
+    return render(request, "blog/login.html", {'form': form})
+
+def logout(request):
+    # Finalizamos la sesión
+    do_logout(request)
+    # Redireccionamos a la portada
+    return redirect('/')
